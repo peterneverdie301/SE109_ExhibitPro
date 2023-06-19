4f5b591ff4c882109a9baf2c2617394c2e9d1c1e
peterneverdie301 <lahuy3012002@gmail.com>
2023-06-20 00:49:13 +0700

    .

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = (env, agrv) => {
          }
       },
       output: {
-         path: path.resolve('build'),
+         path: path.resolve('public/build'),
          publicPath: '/build/',
          filename: 'bundle.js',
          environment: {
